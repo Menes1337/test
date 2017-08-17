2e4ac5802b29ecc3e63573e31ca4a739f92f3cf9
Menes1337 <oliver+github@ebuuk.de>
2017-08-17 21:15:38 +0200

    Changed .travis.yml

---
+++ AbstractList.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace ThreeDCart\Primitive;
+
+abstract class AbstractList
+{
+    /** @var array */
+    protected $list;
+    
+    /**
+     * @param array $list
+     */
+    public function __construct(array $list = [])
+    {
+        $this->list = $list;
+    }
+    
+    /**
+     * @param mixed $entry
+     */
+    protected function addEntry($entry)
+    {
+        $this->list[] = $entry;
+    }
+    
+    public function clear()
+    {
+        $this->list = [];
+    }
+    
+    /**
+     * @return bool
+     */
+    public function isEmpty()
+    {
+        return empty($this->list);
+    }
+    
+    /**
+     * @return int
+     */
+    public final function count()
+    {
+        return count($this->list);
+    }
+}
