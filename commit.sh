cf7ea5c9ebacbc8dc2ac9636671cf5f168d053ed
Menes1337 <oliver+github@ebuuk.de>
2017-08-18 22:14:30 +0200

    test

--- commit.sh
+++ commit.sh
@@ -21,7 +21,7 @@ git config user.name "Travis CI"
 git config user.email "travis@travis-ci.org"
 
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-if git diff; then
+if git diff --quiet; then
     echo "No changes to the output on this push; exiting."
     exit 0
 fi
