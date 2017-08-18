fac0a67d2d700e9a877dafdf9e80714944cbc8da
Oliver Müller <oliver.mueller@shopgate.com>
2017-08-18 16:08:27 +0200

    changed stuff

--- build_zip.sh
+++ build_zip.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 mkdir shopgate-cart-integration-sdk
-#cp ./* shopgate-cart-integration-sdk -R
 rsync -av --progress * shopgate-cart-integration-sdk --exclude shopgate-cart-integration-sdk
 zip -r shopgate-cart-integration-sdk.zip shopgate-cart-integration-sdk
