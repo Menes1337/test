6cd4f8215521b6b98b84722fb07f38850f2b42bf
Oliver Müller <oliver.mueller@shopgate.com>
2017-08-18 15:10:57 +0200

    changed stuff

--- build_zip.sh
+++ build_zip.sh
@@ -3,4 +3,4 @@
 mkdir $TRAVIS_BUILD_DIR/shopgate-cart-integration-sdk
 #cp ./* shopgate-cart-integration-sdk -R
 rsync -av --progress * $TRAVIS_BUILD_DIR/shopgate-cart-integration-sdk --exclude shopgate-cart-integration-sdk
-zip shopgate-cart-integration-sdk
+zip -r shopgate-cart-integration-sdk.zip $TRAVIS_BUILD_DIR/shopgate-cart-integration-sdk
