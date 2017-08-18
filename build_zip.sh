05b25f40d584af6f8bdbb084df04c8b865ccff50
Menes1337 <oliver+github@ebuuk.de>
2017-08-18 20:49:32 +0200

    test

--- build_zip.sh
+++ build_zip.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 mkdir shopgate-cart-integration-sdk
-rsync -av --progress * shopgate-cart-integration-sdk --exclude shopgate-cart-integration-sdk
+rsync -av --exclude={.git,.gitignore,.php-cs-cache,.php-cs.dist,.travis.yml,build_release_package.sh,shopgate-cart-integration-sdk} ./ shopgate-cart-integration-sdk
 zip -r shopgate-cart-integration-sdk.zip shopgate-cart-integration-sdk
